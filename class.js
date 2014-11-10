e045c08f49fd51423eaf8e25be5c159dc0aec72f
Karen <karenpenglabs@gmail.com>
2014-11-10 16:21:44 -0500

    working without hit detection and sound

--- class.js
+++ class.js
@@ -1,26 +1,42 @@
 (function (exports) {
-  function Note(_x, _y, _z, _direction) {
+  function Note(_x, _y, _z, _direction, _scene) {
     this.geometry = new THREE.SphereGeometry(10, 16, 16);
     var color;
-    if (_direction) {
-      color = 0xff0000;
-    } else {
-      color = 0x0000ff;
+    switch (_direction) {
+    case 0:
+      color = 0xffff00;
+      break;
+    case 1:
+      color = 0x00ffff;
+      break;
+    case 2:
+      color = 0xffffff;
     }
-    this.material = new THREE.MeshBasicMaterial({
+    this.material = new THREE.MeshLambertMaterial({
       color: color
     });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.mesh.position.x = _x;
     this.mesh.position.y = _y;
     this.mesh.position.z = _z;
+    _scene.add(this.mesh);
+
+    // var intensity = 2.5;
+    // var distance = 5000;
+    // this.light = new THREE.PointLight(color, intensity, distance);
+    // _scene.add(this.light);
+
     this.direction = _direction;
     this.change = false;
     this.hit = false;
-    this.left = new THREE.Vector3(1, 0, 5);
-    this.right = new THREE.Vector3(-1, 0, 5);
-    //scene.add(this.mesh);
+    this.left = new THREE.Vector3(1, 0, 6);
+    this.right = new THREE.Vector3(-1, 0, 6);
+
   }
+
+  // Note.prototype.play = function(){
+
+  // }
 
   Note.prototype.update = function () {
     if (!this.change) {
@@ -29,19 +45,41 @@
       } else {
         this.mesh.position.add(this.right);
       }
+    } else {
+      this.mesh.position.add(new THREE.Vector3(0, 0, 6));
     }
   };
 
-  function Line(a, b) {
-    var material = new THREE.LineBasicMaterial({
-      color: 0x0000ff
+  function Line(a, b, _direction, _scene) {
+    var color;
+    switch (_direction) {
+    case 0:
+      color = 0xffff00;
+      break;
+    case 1:
+      color = 0x00ffff;
+      break;
+    case 2:
+      color = 0xffffff;
+    }
+    this.material = new THREE.LineBasicMaterial({
+      color: color
     });
-    var geometry = new THREE.Geometry();
-    geometry.vertices.push(a, b);
-    this.mesh = new THREE.Line(geometry, material);
+    this.geometry = new THREE.Geometry();
+    this.geometry.vertices.push(a, b);
+    this.mesh = new THREE.Line(this.geometry, this.material);
+    _scene.add(this.mesh);
+
+    //this.geometry.verticesNeedUpdate = true;
   }
 
-  function Notes(_x, _direction) {
+  Line.prototype.update = function (a, b) {
+    this.geometry.verticesNeedUpdate = true;
+    this.geometry.vertices[0] = a;
+    this.geometry.vertices[1] = b;
+  };
+
+  function Notes(_x, _direction, _scene) {
     this.notes = [];
     this.lines = [];
     this.melody = [-100, -60, 40, 80, 100, -60, -100, 80, 40, -60, 80];
@@ -49,34 +87,84 @@
     this.x = _x;
     this.direction = _direction;
     this.end = new THREE.Vector3(0, 0, -10000);
+    this.scene = _scene;
   }
 
   Notes.prototype.produce = function () {
-    this.notes.push(new Note(this.x, this.melody[this.number], -2600, this.direction));
-    //this.
+    this.notes.push(new Note(this.x, this.melody[this.number], -2600, this.direction, this.scene));
     this.number++;
-    if (this.number > this.melody.length) this.number = 0;
+    if (this.number > this.melody.length) {
+      this.number = 0;
+    }
+    if (this.notes.length > 1) {
+      var pre = this.notes[this.notes.length - 2];
+      var nex = this.notes[this.notes.length - 1];
+      this.lines.push(new Line(pre.mesh.position, nex.mesh.position, this.direction, this.scene));
+    }
   };
 
   Notes.prototype.update = function () {
-    this.notes.forEach(function (note) {
-      note.update();
+    for (var i = 0; i < this.notes.length; i++) {
+      this.notes[i].update();
+      if (i === this.notes.length - 1) {
+        break;
+      }
+      this.lines[i].update(this.notes[i].mesh.position, this.notes[i + 1].mesh.position);
+    }
+  };
+
+  Notes.prototype.hit = function () {
+    this.notes.forEach(function (note, index) {
+      if (note.mesh.position.z > 390) {
+        note.change = true;
+      }
     });
   };
 
   Notes.prototype.destroy = function () {
-    for (var i = 0; i < this.notes.length; i++) {
-      var note = this.notes[i];
-      if (note.position.z > 1000 && note.mesh instanceof THREE.Mesh) {
-        scene.remove(note.mesh);
-        note.geometry.dispose();
-        note.material.dispose();
-        note.mesh = null;
-        note = null;
+    if (this.notes.length > 2) {
+      if (this.notes[0].mesh.position.z > 2000 && this.notes[1].mesh.position.z > 2000) {
+        this.scene.remove(this.notes[0].mesh);
+        this.notes[0].geometry.dispose();
+        this.notes[0].material.dispose();
+        this.notes[0].mesh = null;
+        this.notes[0] = null;
+        this.scene.remove(this.notes[1].mesh);
+        this.notes[1].geometry.dispose();
+        this.notes[1].material.dispose();
+        this.notes[1].mesh = null;
+        this.notes[1] = null;
+
+        //console.log(this.notes.length);
+        //delete the line between them
+        this.scene.remove(this.lines[0].mesh);
+        this.lines[0].geometry.dispose();
+        this.lines[0].material.dispose();
+        this.lines[0].mesh = null;
+        this.lines[0] = null;
+
+        //remove these two balls in the array, also the line between them
+        this.notes.splice(0, 2);
+        this.lines.splice(0, 1);
       }
-      this.notes.splice(i, 1);
     }
   };
 
+  function Hit(_scene) {
+    this.notes = [];
+    var melody = [-100, -60, 40, 80, 100, -60, -100, 80, 40, -60, 80];
+    for (var i = 0; i < 12; i++) {
+      this.notes.push(new Note(0, melody[i], 390, 2, _scene));
+    }
+    var material = new THREE.LineBasicMaterial({
+      color: 0xffffff
+    });
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(0, 500, 390), new THREE.Vector3(0, -500, 390));
+    var mesh = new THREE.Line(geometry, material);
+    _scene.add(mesh);
+  }
+
   exports.Notes = Notes;
+  exports.Hit = Hit;
 })(this);
--- script3.js
+++ script3.js
@@ -4,14 +4,13 @@
   var videoInput = document.getElementById('inputVideo');
   var canvasInput = document.getElementById('inputCanvas');
 
-  // 3d model setup
-
   var container, stats;
   var camera, scene, renderer;
   var plane;
   var leftNotes, rightNotes;
-  var clock;
+  var hit;
   var frameCount = 0;
+  var light1, light2, light3, light4, light5, light6;
 
   function init() {
 
@@ -19,16 +18,88 @@
     document.body.appendChild(container);
 
     scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(0x000000, 1, 10000);
+    scene.fog = new THREE.Fog(0x000000, 1, 1000);
 
     camera = new THREE.PerspectiveCamera(23, window.innerWidth / window.innerHeight, 1, 100000);
     camera.position.z = 6000;
     scene.add(camera);
 
+    //Lights
+    // var light = new THREE.AmbientLight(0xffffff); // soft white light
+    // scene.add(light);
+
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(0, 1, 0);
+    scene.add(directionalLight);
+
+    directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(0, 1, 2000);
+    scene.add(directionalLight);
+
+    var light = new THREE.PointLight(0xaa2222, 3, 50000);
+    light.position.set(100, 100, 100);
+    scene.add(light);
+
+    light = new THREE.PointLight(0x2222aa, 3, 50000);
+    light.position.set(-100, -100, 1000);
+    scene.add(light);
+
+    // var intensity = 2.5;
+    // var distance = 100;
+    // var c1 = 0xff0040,
+    //   c2 = 0x0040ff,
+    //   c3 = 0x80ff80,
+    //   c4 = 0xffaa00,
+    //   c5 = 0x00ffaa,
+    //   c6 = 0xff1100;
+    // //var c1 = 0xffffff, c2 = 0xffffff, c3 = 0xffffff, c4 = 0xffffff, c5 = 0xffffff, c6 = 0xffffff;
+
+    // var sphere = new THREE.SphereGeometry(0.25, 16, 8);
+
+    // light1 = new THREE.PointLight(c1, intensity, distance);
+    // light1.add(new THREE.Mesh(sphere, new THREE.MeshPhongMaterial({
+    //   color: c1
+    // })));
+    // scene.add(light1);
+
+    // light2 = new THREE.PointLight(c2, intensity, distance);
+    // light2.add(new THREE.Mesh(sphere, new THREE.MeshPhongMaterial({
+    //   color: c2
+    // })));
+    // scene.add(light2);
+
+    // light3 = new THREE.PointLight(c3, intensity, distance);
+    // light3.add(new THREE.Mesh(sphere, new THREE.MeshPhongMaterial({
+    //   color: c3
+    // })));
+    // scene.add(light3);
+
+    // light4 = new THREE.PointLight(c4, intensity, distance);
+    // light4.add(new THREE.Mesh(sphere, new THREE.MeshPhongMaterial({
+    //   color: c4
+    // })));
+    // scene.add(light4);
+
+    // light5 = new THREE.PointLight(c5, intensity, distance);
+    // light5.add(new THREE.Mesh(sphere, new THREE.MeshPhongMaterial({
+    //   color: c5
+    // })));
+    // scene.add(light5);
+
+    // light6 = new THREE.PointLight(c6, intensity, distance);
+    // light6.add(new THREE.Mesh(sphere, new THREE.MeshPhongMaterial({
+    //   color: c6
+    // })));
+    // scene.add(light6);
+
+    // var dlight = new THREE.DirectionalLight(0xffffff, 0.1);
+    // dlight.position.set(0.5, -1, 0).normalize();
+    // scene.add(dlight);
+
     // Planes
 
     //top wall
-    plane1 = new THREE.Mesh(new THREE.PlaneGeometry(1000, 3000, 5, 15), new THREE.MeshBasicMaterial({
+    plane1 = new THREE.Mesh(new THREE.PlaneGeometry(1000, 3000, 5, 15), new THREE.MeshPhongMaterial({
       color: 0xcccccc,
       wireframe: true
     }));
@@ -38,9 +109,8 @@
     scene.add(plane1);
 
     //left wall
-    plane2 = new THREE.Mesh(new THREE.PlaneGeometry(3000, 1000, 15, 5), new THREE.MeshBasicMaterial({
-      color: 0xcccccc,
-      wireframe: true
+    plane2 = new THREE.Mesh(new THREE.PlaneGeometry(3000, 1000, 15, 5), new THREE.MeshPhongMaterial({
+      color: 0xcccccc
     }));
     plane2.rotation.y = Math.PI / 2;
     plane2.position.x = -500;
@@ -48,9 +118,8 @@
     scene.add(plane2);
 
     //right wall
-    plane3 = new THREE.Mesh(new THREE.PlaneGeometry(3000, 1000, 15, 5), new THREE.MeshBasicMaterial({
-      color: 0xcccccc,
-      wireframe: true
+    plane3 = new THREE.Mesh(new THREE.PlaneGeometry(3000, 1000, 15, 5), new THREE.MeshPhongMaterial({
+      color: 0xcccccc
     }));
     plane3.rotation.y = -Math.PI / 2;
     plane3.position.x = 500;
@@ -58,7 +127,7 @@
     scene.add(plane3);
 
     //bottom wall
-    plane4 = new THREE.Mesh(new THREE.PlaneGeometry(1000, 3000, 5, 15), new THREE.MeshBasicMaterial({
+    plane4 = new THREE.Mesh(new THREE.PlaneGeometry(1000, 3000, 5, 15), new THREE.MeshPhongMaterial({
       color: 0xcccccc,
       wireframe: true
     }));
@@ -68,9 +137,13 @@
     scene.add(plane4);
 
     //notes
-    leftNotes = new Notes(-500, 1);
-    rightNotes = new Notes(500, 0);
+    leftNotes = new Notes(-500, 1, scene);
+    rightNotes = new Notes(500, 0, scene);
 
+    //hiting notes
+    hit = new Hit(scene);
+
+    //renderer
     renderer = new THREE.WebGLRenderer({
       clearAlpha: 1
     });
@@ -82,9 +155,6 @@
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.top = '0px';
     document.body.appendChild(stats.domElement);
-
-    clock = new THREE.Clock();
-    clock.start();
 
     window.addEventListener('resize', onWindowResize, false);
 
@@ -102,16 +172,38 @@
     frameCount++;
     stats.update();
 
-    //console.log(clock.getElapsedTime());
+    // var time = Date.now() * 0.00025;
+    // var z = 20,
+    //   d = 150;
 
-    if (frameCount % 60 === 0) {
+    // light1.position.x = Math.sin(time * 0.7) * d;
+    // light1.position.z = Math.cos(time * 0.3) * d;
+
+    // light2.position.x = Math.cos(time * 0.3) * d;
+    // light2.position.z = Math.sin(time * 0.7) * d;
+
+    // light3.position.x = Math.sin(time * 0.7) * d;
+    // light3.position.z = Math.sin(time * 0.5) * d;
+
+    // light4.position.x = Math.sin(time * 0.3) * d;
+    // light4.position.z = Math.sin(time * 0.5) * d;
+
+    // light5.position.x = Math.cos(time * 0.3) * d;
+    // light5.position.z = Math.sin(time * 0.5) * d;
+
+    // light6.position.x = Math.cos(time * 0.7) * d;
+    // light6.position.z = Math.cos(time * 0.5) * d;
+
+    if (frameCount % 30 === 0) {
       leftNotes.produce();
-      scene.add(leftNotes.notes[leftNotes.notes.length - 1].mesh);
-      //rightNotes.produce();
-      //scene.add(rightNotes.notes[rightNotes.notes.length - 1].mesh);
+      rightNotes.produce();
     }
     leftNotes.update();
-    //rightNotes.update();
+    leftNotes.hit();
+    leftNotes.destroy();
+    rightNotes.update();
+    rightNotes.hit();
+    rightNotes.destroy();
 
     renderer.render(scene, camera);
   }
